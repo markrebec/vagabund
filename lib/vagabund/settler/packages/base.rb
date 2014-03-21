4fdec29af57c7cce47b35561bd9d476021007c5d
Mark Rebec <mark@markrebec.com>
2014-03-21 12:06:39 -0700

    refactors build/extract/pull logic into separate protected methods

--- lib/vagabund/settler/packages/base.rb
+++ lib/vagabund/settler/packages/base.rb
@@ -8,69 +8,31 @@ module Vagabund
           pull machine
           extract machine
 
-          machine.ui.info "Building package #{name}-#{version} in #{build_path}..."
-          # go into dir
           if @options.respond_to? :builder
+            machine.ui.info "Building package #{name}-#{version} with custom builder..."
             @options.builder.call(self, machine, machine.communicate)
           else
-            begin
-              machine.communicate.execute "cd #{build_path}; ./configure && make"
-              machine.communicate.sudo "cd #{build_path}; make install"
-            rescue
-              raise Vagrant::Errors::VagrantError, :package_build_error
-            end
+            machine.ui.info "Building package #{name}-#{version} in #{build_path}..."
+            build_package machine
           end
         end
 
         def extract(machine)
-          local_ext = File.extname(local_file)
-
           if @options.respond_to? :extractor
             machine.ui.info "Unpacking #{local_file} with custom extractor..."
             @options.extractor.call(self, machine, machine.communicate)
           else
-            
             machine.ui.info "Unpacking #{local_file}..."
-            # TODO this should better handle different combinations. For example right now a .tar.zip wouldn't work, a .gz wouldn't work, but a .zip or a .tar.gz DOES work
-            case local_ext
-            when '.gz', '.bz', '.bz2', '.xz'
-              machine.communicate.sudo "rm -rf #{File.join(File.dirname(local_file), File.basename(local_file, local_ext))}"
-              
-              case local_ext
-              when '.gz'
-                machine.communicate.execute "cd #{File.dirname(local_file)}; gunzip #{local_file}"
-              when '.bz', '.bz2'
-                machine.communicate.execute "cd #{File.dirname(local_file)}; bunzip2 #{local_file}"
-              when '.xz'
-                machine.communicate.execute "cd #{File.dirname(local_file)}; xz -d #{local_file}"
-              end
-              
-              @local_file = File.join(File.dirname(local_file), File.basename(local_file, local_ext))
-              extract(machine) if ['.gz', '.bz', '.bz2', '.xz', '.zip', '.tar'].include?(File.extname(local_file))
-            when '.zip'
-              machine.communicate.execute "cd #{File.dirname(local_file)}; unzip #{local_file} -d #{build_path}"
-              machine.communicate.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/* ./") rescue nil
-              machine.communicate.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/.* ./") rescue nil
-              machine.communicate.execute("cd #{build_path}; rm -rf #{File.basename(local_file, local_ext)}") rescue nil
-            when '.tar'
-              machine.communicate.execute "mkdir -p #{build_path}"
-              begin
-                machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} #{File.basename(local_file, local_ext)} -C #{build_path}"
-              rescue
-                begin
-                  machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} #{name}-#{version} -C #{build_path}"
-                rescue
-                  machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} -C #{build_path}"
-                end
-              end
-            end
-          
+            extract_package machine
           end
         end
 
         def pull(machine)
-          machine.communicate.sudo "rm -rf #{local_file} #{build_path}"
-          source.pull machine, local_file
+          if @options.respond_to? :puller
+            @options.puller.call(self, machine, machine.communicate)
+          else
+            pull_package machine
+          end
         end
 
         def local_file
@@ -108,6 +70,62 @@ module Vagabund
           #elsif @options.respond_to?(:scp)
             # remote scp
           end
+        end
+
+        def build_package(machine)
+          begin
+            machine.communicate.execute "cd #{build_path}; ./configure && make"
+            machine.communicate.sudo "cd #{build_path}; make install"
+          rescue
+            raise Vagrant::Errors::VagrantError, :package_build_error
+          end
+        end
+
+        def extract_package(machine)
+          if File.directory?(local_file)
+            machine.communicate.execute "cp -r #{local_file} #{build_path}" if local_file != build_path
+          else
+            local_ext = File.extname(local_file)
+
+            # TODO this should better handle different combinations. For example right now a .tar.zip wouldn't work, a .gz wouldn't work, but a .zip or a .tar.gz DOES work
+            case local_ext
+            when '.gz', '.bz', '.bz2', '.xz'
+              machine.communicate.sudo "rm -rf #{File.join(File.dirname(local_file), File.basename(local_file, local_ext))}"
+              
+              case local_ext
+              when '.gz'
+                machine.communicate.execute "cd #{File.dirname(local_file)}; gunzip #{local_file}"
+              when '.bz', '.bz2'
+                machine.communicate.execute "cd #{File.dirname(local_file)}; bunzip2 #{local_file}"
+              when '.xz'
+                machine.communicate.execute "cd #{File.dirname(local_file)}; xz -d #{local_file}"
+              end
+              
+              @local_file = File.join(File.dirname(local_file), File.basename(local_file, local_ext))
+              extract_package(machine) if ['.gz', '.bz', '.bz2', '.xz', '.zip', '.tar'].include?(File.extname(local_file))
+            when '.zip'
+              machine.communicate.execute "cd #{File.dirname(local_file)}; unzip #{local_file} -d #{build_path}"
+              machine.communicate.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/* ./") rescue nil
+              machine.communicate.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/.* ./") rescue nil
+              machine.communicate.execute("cd #{build_path}; rm -rf #{File.basename(local_file, local_ext)}") rescue nil
+            when '.tar'
+              machine.communicate.execute "mkdir -p #{build_path}"
+              begin
+                machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} #{File.basename(local_file, local_ext)} -C #{build_path}"
+              rescue
+                begin
+                  machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} #{name}-#{version} -C #{build_path}"
+                rescue
+                  machine.communicate.execute "cd #{File.dirname(local_file)}; tar xf #{local_file} -C #{build_path}"
+                end
+              end
+            end
+          end
+        end
+
+        def pull_package(machine)
+          machine.communicate.sudo "rm -rf #{local_file} #{build_path}"
+          source.pull machine, local_file
         end
 
       end
