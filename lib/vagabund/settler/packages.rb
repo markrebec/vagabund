2fdc2a9efd693dfcccfde3be8c3331b8f03b7696
Mark Rebec <mark@markrebec.com>
2014-03-24 15:20:18 -0700

    fixes a bunch of references to local package and build root, directly re-executes nested extractor procs

--- lib/vagabund/settler/packages.rb
+++ lib/vagabund/settler/packages.rb
@@ -10,66 +10,69 @@ module Vagabund
       EXTENSIONS = ['.gz', '.bz', '.bz2', '.xz', '.zip', '.tar', '.tgz', '.tbz', '.tbz2', '.txz']
 
       BUILDER = Proc.new do |package, machine, channel|
-        channel.execute "cd #{build_path}; ./configure && make"
+        execute "cd #{build_path}; ./configure && make"
       end
 
       CLEANER = Proc.new do |package, machine, channel|
-        channel.sudo "rm -rf #{build_root}"
+        sudo "rm -rf #{build_root}"
       end
 
       EXTRACTOR = Proc.new do |package, machine, channel|
         if build_path_exists?(machine)
           machine.ui.warn "Build path #{build_path} already exists, using it for the build. If you would like to use a clean source tree, you should manually remove it and run `vagrant provision` again."
-        elsif File.directory?(local_file)
-          channel.execute "cp -r #{local_file} #{build_path}" if local_file != build_path
+        elsif File.directory?(local_package)
+          execute "cp -r #{local_package} #{build_path}" if local_package != build_path
         else
-          channel.execute "mkdir -p #{build_path}"
-          local_ext = File.extname(local_file)
+          execute "mkdir -p #{build_path}"
+          local_ext = File.extname(local_package)
 
           case local_ext
           when '.gz'
-            channel.execute "cd #{build_root}; gzip -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}"
+            execute "cd #{build_root}; gzip -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}"
           when '.tgz'
-            channel.execute "cd #{build_root}; gzip -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}.tar"
+            execute "cd #{build_root}; gzip -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}.tar"
           when '.bz', '.bz2'
-            channel.execute "cd #{build_root}; bzip2 -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}"
+            execute "cd #{build_root}; bzip2 -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}"
           when '.tbz', '.tbz2'
-            channel.execute "cd #{build_root}; bzip2 -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}.tar"
+            execute "cd #{build_root}; bzip2 -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}.tar"
           when '.xz'
-            channel.execute "cd #{build_root}; xz -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}"
+            execute "cd #{build_root}; xz -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}"
           when '.txz'
-            channel.execute "cd #{build_root}; xz -dc #{local_file} > #{build_path}/#{File.basename(local_file, local_ext)}.tar"
+            execute "cd #{build_root}; xz -dc #{local_package} > #{build_path}/#{File.basename(local_package, local_ext)}.tar"
           when '.zip'
-            channel.execute "cd #{build_root}; unzip #{local_file} -d #{build_path}"
-            channel.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/* ./") rescue nil
-            channel.execute("cd #{build_path}; mv #{File.basename(local_file, local_ext)}/.* ./") rescue nil
-            channel.execute("cd #{build_path}; mv #{name}-#{version}/* ./") rescue nil
-            channel.execute("cd #{build_path}; mv #{name}-#{version}/.* ./") rescue nil
-            channel.execute("cd #{build_path}; rm -rf #{File.basename(local_file, local_ext)}") rescue nil
+            execute "cd #{build_root}; unzip #{local_package} -d #{build_path}"
+            execute("cd #{build_path}; mv #{File.basename(local_package, local_ext)}/* ./") rescue nil
+            execute("cd #{build_path}; mv #{File.basename(local_package, local_ext)}/.* ./") rescue nil
+            execute("cd #{build_path}; mv #{name}-#{version}/* ./") rescue nil
+            execute("cd #{build_path}; mv #{name}-#{version}/.* ./") rescue nil
+            execute("cd #{build_path}; rm -rf #{File.basename(local_package, local_ext)}") rescue nil
           when '.tar'
             begin
-              channel.execute "cd #{build_root}; tar xf #{local_file} #{File.basename(local_file, local_ext)} -C #{build_path}"
+              execute "cd #{build_root}; tar xf #{local_package} #{File.basename(local_package, local_ext)} -C #{build_path}"
             rescue
               begin
-                channel.execute "cd #{build_root}; tar xf #{local_file} #{name}-#{version} -C #{build_path}"
+                execute "cd #{build_root}; tar xf #{local_package} #{name}-#{version} -C #{build_path}"
               rescue
-                channel.execute "cd #{build_root}; tar xf #{local_file} -C #{build_path}"
+                execute "cd #{build_root}; tar xf #{local_package} -C #{build_path}"
               end
             end
           end
 
           build_files = ""
-          channel.execute "cd #{build_path}; ls" do |type, data|
+          execute "cd #{build_path}; ls" do |type, data|
             build_files = data
           end
 
           if build_files.split($/).length == 1
             new_package_file = File.basename(build_files.chomp)
             if Package::EXTENSIONS.include?(File.extname(new_package_file))
-              channel.execute "mv #{File.join(build_path, new_package_file)} #{build_root}"
-              channel.sudo    "rm -rf #{local_file} #{build_path}"
-              config.local_file = File.join(build_root, new_package_file)
-              extract(machine)
+              execute "mv #{File.join(build_path, new_package_file)} #{build_root}"
+              sudo    "rm -rf #{local_package} #{build_path}"
+              config.local_package = File.join(build_root, new_package_file)
+              
+              # Re-execute this proc directly instead of going back through extract() or action_exec()
+              detail "Unpacking #{local_package}..."
+              instance_exec self, machine, machine.communicate, &EXTRACTOR
             end
           end
 
@@ -77,15 +80,15 @@ module Vagabund
       end
 
       INSTALLER = Proc.new do |package, machine, channel|
-        channel.sudo "cd #{build_path}; make install"
+        sudo "cd #{build_path}; make install"
       end
 
       PULLER = Proc.new do |package, machine, channel|
         if package_exists?(machine)
-          config.local_file = existing_package_file(machine)
-          machine.ui.warn "Package #{local_file} already exists, using it for the build. If you would like to re-download the package, you should manually remove it then run `vagrant provision` again."
+          config.local_package = existing_package_file(machine)
+          machine.ui.warn "Package #{local_package} already exists, using it for the build. If you would like to re-download the package, you should manually remove it then run `vagrant provision` again."
         else
-          source.pull machine, local_file
+          source.pull machine, local_package
         end
       end
 
